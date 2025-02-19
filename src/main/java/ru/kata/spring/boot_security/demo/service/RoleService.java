6163c89602c207bd47ab3c54b9cb2eb089d9604c
Anastasia <nastya.solovyeva.99@gmail.com>
2025-02-19 15:42:30 +0300

    обновила

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
@@ -1,8 +1,12 @@
 package ru.kata.spring.boot_security.demo.service;
 
 import ru.kata.spring.boot_security.demo.model.Role;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface RoleService {
+    List<Role> getAllRoles();
+
     Optional<Role> getNameRoles(String name);
 }
